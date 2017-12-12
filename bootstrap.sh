7bc927f023fadf7fed06a59933f943b53012f475
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2017-12-12 22:46:11 +0100

    Added more packets in base image to reduce build time.
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sudo pkg update
-sudo pkg install -y openjdk8
+sudo pkg upgrade
+sudo pkg install -y openjdk8 doxygen python python3 py27-numpy py27-scipy py36-numpy py36-scipy cmake ninja lcov cppcheck gcc
 
